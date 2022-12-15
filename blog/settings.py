4033c09620cd2bf866de4655d7592e72685fe4d2
BoloyNag <boloy.nag99@gmail.com>
2022-12-15 19:59:58 +0530

    Adding allowed host for webapp

--- blog/settings.py
+++ blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'u4#8ba@2!()=2y-=c*xxf0k2rv-s#3ya(tkrvmihieed^qw+_f'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['boloynag.pythonanywhere.com']
 
 
 # Application definition
